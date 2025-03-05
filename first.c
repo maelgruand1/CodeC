a44d8bead3423212137b365aba6d2cbd504ad0e9
Maël Gruand <maelgruand1@yahoo.com>
2025-03-05 10:59:40 +0100

    send code to github

--- first.c
+++ first.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    char *username = "Mael";
-    char *password = "1234";
+    char *username = "maelgruand";
+    char *user = "Mael";
+    char *password = "23092006";
 
     printf("Enter your username: ");
     char input_username[100];
@@ -12,14 +14,26 @@ int main()
     printf("Enter your password: ");
     char input_password[100];
     scanf("%s", input_password);
+    char *verifed = verifUser(username, user, password, input_username, input_password);
+    if (verifed == "yes"){
+        printf("Welcome %s\n", user);
+    }
+    else{
+        printf("Invalid username or password\n");
+    }
+    return 0;
+}
+char *verifUser(username, user, password, input_username, input_password)
+{
 
     if (strcmp(username, input_username) == 0 && strcmp(password, input_password) == 0)
     {
-        printf("Welcome %s\n", username);
+        printf("Welcome %s\n", user);
     }
     else
     {
         printf("Invalid username or password\n");
+        return "no";
     }
-    return 0;
+    return "yes";
 }
